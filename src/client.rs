3a914054f01257d61fa13e7882b543745f2ce247
Spencer Sellers <spencesellers@gmail.com>
2021-06-04 19:24:50 -0500

    Create monitoring thread for client

--- src/client.rs
+++ src/client.rs
@@ -1,26 +1,47 @@
 use std::net::UdpSocket;
 use crate::messages::build_message;
+use std::sync::{Mutex, Arc};
+use std::thread;
+use std::time::Duration;
 
 pub fn run_test_client(dest: &str) {
     let socket = UdpSocket::bind("0.0.0.0:0").expect("Could not bind to address");
     let mut random = rand::thread_rng();
 
-    ctrlc::set_handler(move || {
-        println!("Quitting");
-        std::process::exit(0);
-    }).expect("Could not set ctrl-c handler.");
+    let count = Arc::new(Mutex::new(0u64));
+    set_ctrlc_handler(&count);
+    start_monitoring_thread(&count);
 
-    let mut count = 0u64;
     println!("Sending to {}", dest);
     loop {
-        count += 1;
         let buf = build_message(&mut random);
 
         socket.send_to(&buf, dest).expect("Could not send");
-        if count % 10_000 == 0 {
-            println!("Sent {} datagrams", count);
-        }
+        let mut count = count.lock().unwrap();
+        *count += 1;
     }
+}
+
+fn start_monitoring_thread(count: &Arc<Mutex<u64>>) {
+    let our_count = count.clone();
+    thread::spawn(move || {
+        loop {
+            {
+                let count = our_count.lock().unwrap();
+                println!("Sent {} datagrams", count);
+            }
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+}
+
+fn set_ctrlc_handler(count: &Arc<Mutex<u64>>) {
+    let our_count = count.clone();
+    ctrlc::set_handler(move || {
+        println!("\nQuitting");
+        println!("Sent {} datagrams", our_count.lock().unwrap());
+        std::process::exit(0);
+    }).expect("Could not set ctrl-c handler.");
 }
 
 
