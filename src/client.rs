25d02f4864cc83db583c73e1b5506274c5ad0f42
Spencer Sellers <spencesellers@gmail.com>
2021-06-04 19:28:56 -0500

    Create a client state struct to handle future changes better

--- src/client.rs
+++ src/client.rs
@@ -8,38 +8,42 @@ pub fn run_test_client(dest: &str) {
     let socket = UdpSocket::bind("0.0.0.0:0").expect("Could not bind to address");
     let mut random = rand::thread_rng();
 
-    let count = Arc::new(Mutex::new(0u64));
-    set_ctrlc_handler(&count);
-    start_monitoring_thread(&count);
+    let state = Arc::new(Mutex::new(ClientState { count: 0 }));
+    set_ctrlc_handler(&state);
+    start_monitoring_thread(&state);
 
     println!("Sending to {}", dest);
     loop {
         let buf = build_message(&mut random);
 
         socket.send_to(&buf, dest).expect("Could not send");
-        let mut count = count.lock().unwrap();
-        *count += 1;
+        let mut state = state.lock().unwrap();
+        state.count += 1;
     }
 }
 
-fn start_monitoring_thread(count: &Arc<Mutex<u64>>) {
-    let our_count = count.clone();
+struct ClientState {
+    count: u64,
+}
+
+fn start_monitoring_thread(state: &Arc<Mutex<ClientState>>) {
+    let our_state = state.clone();
     thread::spawn(move || {
         loop {
             {
-                let count = our_count.lock().unwrap();
-                println!("Sent {} datagrams", count);
+                let state = our_state.lock().unwrap();
+                println!("Sent {} datagrams", state.count);
             }
             thread::sleep(Duration::from_secs(1));
         }
     });
 }
 
-fn set_ctrlc_handler(count: &Arc<Mutex<u64>>) {
-    let our_count = count.clone();
+fn set_ctrlc_handler(state: &Arc<Mutex<ClientState>>) {
+    let our_state = state.clone();
     ctrlc::set_handler(move || {
         println!("\nQuitting");
-        println!("Sent {} datagrams", our_count.lock().unwrap());
+        println!("Sent {} datagrams", our_state.lock().unwrap().count);
         std::process::exit(0);
     }).expect("Could not set ctrl-c handler.");
 }
