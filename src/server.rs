bd9b469260b2532172b4ffd863dee3c60343866f
Spencer Sellers <spencesellers@gmail.com>
2021-06-06 15:58:01 -0500

    Refactor server logic a bit

--- src/server.rs
+++ src/server.rs
@@ -2,19 +2,14 @@ use std::net::UdpSocket;
 use std::sync::{Mutex, Arc};
 use std::thread;
 use std::time::Duration;
-use crate::messages::{validate_message, MESSAGE_SIZE};
+use crate::messages::{validate_message, MESSAGE_SIZE, DecodedMessage};
 use crate::proc_net;
 use std::str::FromStr;
 
 pub fn run_test_server(port: &str) {
     let socket = UdpSocket::bind(format!("0.0.0.0:{}",port)).expect("Could not bind to address");
     println!("Listening on port {}", port);
-    let status = Arc::new(Mutex::new(ExperimentStatus {
-        good_count: 0,
-        bad_count: 0,
-        last_sequence_number: None,
-        out_of_order: 0
-    }));
+    let status = Arc::new(Mutex::new(ExperimentStatus::new()));
 
     let experiment = Experiment { port: u16::from_str(port).expect("Could not parse port")};
 
@@ -25,15 +20,7 @@ pub fn run_test_server(port: &str) {
         let decoded_message = validate_message(&buf);
 
         let mut status = status.lock().unwrap();
-        if let Some(msg) = decoded_message {
-            status.good_count += 1;
-            if status.last_sequence_number.is_some() && msg.sequence_number != status.last_sequence_number.unwrap() + 1 {
-                status.out_of_order += 1;
-            }
-            status.last_sequence_number = Some(msg.sequence_number);
-        } else {
-            status.bad_count += 1;
-        }
+        status.encounter_message(&decoded_message);
     }
 }
 
@@ -43,6 +30,29 @@ struct ExperimentStatus {
     bad_count: u64,
     last_sequence_number: Option<u32>,
     out_of_order: u64,
+}
+
+impl ExperimentStatus {
+    fn new() -> Self {
+        ExperimentStatus {
+            good_count: 0,
+            bad_count: 0,
+            last_sequence_number: None,
+            out_of_order: 0
+        }
+    }
+
+    fn encounter_message(&mut self, msg: &Option<DecodedMessage>) {
+        if let Some(msg) = msg {
+            self.good_count += 1;
+            if self.last_sequence_number.is_some() && msg.sequence_number != self.last_sequence_number.unwrap() + 1 {
+                self.out_of_order += 1;
+            }
+            self.last_sequence_number = Some(msg.sequence_number);
+        } else {
+            self.bad_count += 1;
+        }
+    }
 }
 
 #[derive(Clone)]
